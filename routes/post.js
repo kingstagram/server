162d8b16625b39c757bd26dc788b91f31272c459
devitasari <devitas700@gmail.com>
2019-12-05 19:58:07 +0700

    post without gcs upload

--- routes/post.js
+++ routes/post.js
@@ -1,9 +1,25 @@
 const router = require('express').Router()
 const PostController = require('../controllers/postController')
+const gcsUpload = require('gcs-upload')
+const { authentication, authorization } = require('../middlewares/auth')
 
+const upload = gcsUpload({
+    limits: {
+      fileSize: 1e6 // in bytes
+    },
+    gcsConfig: {
+      keyFilename: '../keyfile.json',
+      bucketName: process.env.BUCKET_NAME
+    }
+  })
+
+
+router.use( authentication )
 router.post('/add', PostController.addPost)
-router.get('/user', PostController.showAll)
-router.get('')
-
+router.get('/all', PostController.showAll)
+router.get('/mine', PostController.showPostUser)
+router.put('/like/:postId', PostController.likeDislike)
+router.put('/:postId', authorization, PostController.update)
+router.delete('/:postId', authorization, PostController.delete)
 
 module.exports = router
