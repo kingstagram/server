bd07e21ac9ac073aa8e0a5a19cf11eac51b8d70f
devitasari <devitas700@gmail.com>
2019-12-05 19:00:01 +0700

    add routing server

--- routes/post.js
+++ routes/post.js
@@ -1,5 +1,9 @@
 const router = require('express').Router()
+const PostController = require('../controllers/postController')
 
+router.post('/add', PostController.addPost)
+router.get('/user', PostController.showAll)
+router.get('')
 
 
 module.exports = router
