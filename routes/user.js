970cca240557e1732a16be2163ee8e35c18700d4
michaelwp <michael.wenceslaus@gmail.com>
2019-12-06 15:02:19 +0700

    add route find user

--- routes/user.js
+++ routes/user.js
@@ -5,11 +5,8 @@ const {authentication, authorization} = require('../middlewares/auth');
 router.post('/register', userController.register);
 router.post('/login', userController.login);
 router.get('/', userController.viewUser);
-<<<<<<< HEAD
 router.get('/:id', userController.findUser);
-=======
 router.get('/user', authentication, userController.myProfile)
->>>>>>> fc26a03bc92b4326b8fabea697da81e17af1b316
 
 
 module.exports = router;
