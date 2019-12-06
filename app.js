b514f5abe9160a931d25176e1fb5be975e7bdb7c
michaelwp <michael.wenceslaus@gmail.com>
2019-12-06 07:06:17 +0700

    add atlas mongodb uri

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-if (process.env.NODE_ENV == 'development') require('dotenv').config()
+// if (process.env.NODE_ENV == 'development') require('dotenv').config()
+
+require('dotenv').config();
 
 //setup mongoose
-const mongoose = require('mongoose')
-const uris = process.env.URIS
+const mongoose = require('mongoose');
+const uris = process.env.URIS;
 
 mongoose.connect(uris, {
     useCreateIndex: true,
@@ -15,24 +17,24 @@ mongoose.connect(uris, {
     })
     .catch(_=> {
         console.log('db disconnected')
-    })
+    });
 
 //setup express
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const morgan = require('morgan')
-const router = require('./routes')
-const errorHandler = require('./middlewares/errorHandler')
-const port = process.env.PORT
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const morgan = require('morgan');
+const router = require('./routes');
+const errorHandler = require('./middlewares/errorHandler');
+const port = process.env.PORT;
 
-app.use(express.urlencoded({extended: false}))
-app.use(express.json())
-app.use(cors())
-app.use(morgan('dev'))
-app.use('/', router)
-app.use(errorHandler)
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'));
+app.use('/', router);
+app.use(errorHandler);
 
 app.listen(port, _=> {
     console.log('listening port', port)
-})
+});
